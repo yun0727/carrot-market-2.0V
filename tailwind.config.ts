c1c34f291a1768ff3b1346f1931ab9796bfe77e8
yun <austru00@gmail.com>
2024-10-17 07:20:24 +0900

    4.0 Home Screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,20 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend:{
-      margin:{
-        tomato:"120px"
+    extend: {
+      margin: {
+        tomato: "120px",
       },
-      borderRadius:{
-        "sexy-name":"11.11px"
-      }
-    }
+      borderRadius: {
+        "sexy-name": "11.11px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
+
+
+
+
 export default config;
+
