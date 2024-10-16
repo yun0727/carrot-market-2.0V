a08a847f1bbb7f3197f9f0498065728225a15baa
yun <austru00@gmail.com>
2024-10-16 19:44:30 +0900

    3.10 JIT

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    extend:{
+      margin:{
+        tomato:"120px"
+      },
+      borderRadius:{
+        "sexy-name":"11.11px"
+      }
+    }
   },
   plugins: [],
 };
