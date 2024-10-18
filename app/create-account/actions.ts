1d027a3089516d92747c8b9bf8f15ca825cadb7a
yun <austru00@gmail.com>
2024-10-19 02:15:21 +0900

    6.2 Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,12 +2,30 @@
 
 import { z } from "zod"
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10)
+const checkUsernmae = (username:string)=>
+  !username.includes("potato");
+
+const checkPasswords = ({password, confirm_password} : {password:string, confirm_password:string})=> 
+  password === confirm_password
+
+const formSchema = z
+  .object({
+    username: z
+      .string({invalid_type_error:"Username must be a string!",
+        
+        required_error:"Where is my username?"
+        
+      })
+      .min(3,"Way too short!!!")
+      .max(10,"That is too longggg!")
+      .refine(checkUsernmae,"No potatoes allowed"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10)
 })
+.refine(checkPasswords, {
+  message:"Both passwords should be the same!",
+  path:["confirm_password"]})
 
 export async function createAccount(prevState: any , formData:FormData){
   const data = {
