8bd866d9282fb08779c7e821ff8c58611fa89ada
yun <austru00@gmail.com>
2024-10-19 01:36:48 +0900

    6.1 Validation Errors

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -18,26 +18,28 @@ export default function CreateAccount() {
           type="text"
           placeholder="Username"
           required
-          errors={[]}
+          errors={state?.fieldErrors.username}
         />
         <FormInput
           name="email"
           type="email"
           placeholder="Email"
           required
-          errors={[]}
+          errors={state?.fieldErrors.email}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
+          errors={state?.fieldErrors.password}
         />
         <FormInput
           name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
+          errors={state?.fieldErrors.confirm_password}
         />
         <FormButton text="Create account" />
       </form>
